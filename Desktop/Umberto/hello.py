3113f82cd69b044c69d98097fb6ceed0122b8217
bigdive <bigdive@top-ix.org>
2016-10-07 17:04:59 +0200

    First commit

---
+++ Desktop/Umberto/hello.py
@@ -0,0 +1,15 @@
+#Come creare script python al di fuori di py.notebook
+#apro blocco note da alto a sinistra, accessori, etc...
+#lo salvo con l'estensione ".py"
+#apro linea di comando
+#vado dove ho salvato il file (ti conviene sul desktop.. entri nella cartella con cd Nome, usa ls per vedere che c'e' nella cartella)
+#scrivi python nome_script.py e te lo esegue
+
+print "ciao!"
+
+def hello():
+	print "hello di hello!"
+
+#voglio utilizzare questa funzione nello script di prima (chiamato script.py)
+
+
---
+++ Desktop/Umberto/script.py
@@ -0,0 +1,8 @@
+#Come creare script python al di fuori di py.notebook
+#apro blocco note da alto a sinistra, accessori, etc...
+#lo salvo
+#print "ciao!"
+
+#richiamo da altro file
+from hello import hello
+print hello()
